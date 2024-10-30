f1dd38cc0409b5a677177da040bdd1e07c3b5bf2
Nahim Hossain Shohan <nh.shohan6@gmail.com>
2024-10-30 20:26:20 +0600

    Dashboard layout created

---
+++ src/lib/sildeLinks.js
@@ -0,0 +1,83 @@
+import {
+  BookOpenText,
+  Books,
+  CalendarDots,
+  ChalkboardTeacher,
+  Exam,
+  GraduationCap,
+  ListChecks,
+  Megaphone,
+  Notebook,
+  NotePencil,
+  SquaresFour,
+  Users,
+} from "@phosphor-icons/react";
+import { RiParentLine } from "react-icons/ri";
+
+export const links = [
+  {
+    label: "dashboard",
+    href: "/",
+    Icon: <SquaresFour className="w-6 h-6" />,
+  },
+  {
+    label: "Students",
+    href: "/",
+    Icon: <Users className="w-6 h-6" />,
+  },
+  {
+    label: "Teachers",
+    href: "/",
+    Icon: <GraduationCap className="w-6 h-6" />,
+  },
+  {
+    label: "Parents",
+    href: "/",
+    Icon: <RiParentLine className="w-6 h-6" />,
+  },
+  {
+    label: "Subjects",
+    href: "/",
+    Icon: <Books className="w-6 h-6" />,
+  },
+  {
+    label: "Classes",
+    href: "/",
+    Icon: <ChalkboardTeacher className="w-6 h-6" />,
+  },
+  {
+    label: "Lessons",
+    href: "/",
+    Icon: <BookOpenText className="w-6 h-6" />,
+  },
+  {
+    label: "Exams",
+    href: "/",
+    Icon: <NotePencil className="w-6 h-6" />,
+  },
+  {
+    label: "Assignments",
+    href: "/",
+    Icon: <Notebook className="w-6 h-6" />,
+  },
+  {
+    label: "Results",
+    href: "/",
+    Icon: <Exam className="w-6 h-6" />,
+  },
+  {
+    label: "Attendence",
+    href: "/",
+    Icon: <ListChecks className="w-6 h-6" />,
+  },
+  {
+    label: "Events",
+    href: "/",
+    Icon: <CalendarDots className="w-6 h-6" />,
+  },
+  {
+    label: "Announcments",
+    href: "/",
+    Icon: <Megaphone className="w-6 h-6" />,
+  },
+];
