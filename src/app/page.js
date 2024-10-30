d592966123245710c2cf2ba57414c712647a2e35
Nahim Hossain Shohan <nh.shohan6@gmail.com>
2024-10-30 13:54:44 +0600

    tanjim branch started

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1>Shohan Branch</h1>
+      <h1>Tanjim Branch</h1>
     </div>
   );
 }
